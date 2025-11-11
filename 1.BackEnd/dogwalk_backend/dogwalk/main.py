8e390f5fcaefa4f9c4b2bbc19461b43aae3c8514
junyounglee-notebook <younghwen@naver.com>
2025-11-11 12:06:30 +0900

    import dotenv

--- 1.BackEnd/dogwalk_backend/dogwalk/main.py
+++ 1.BackEnd/dogwalk_backend/dogwalk/main.py
@@ -1,7 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
 import os
 
-# Open API Key 불러오기
+load_dotenv()  # 프로젝트 루트의 .env 파일 읽어서 환경변수 등록
+
 api_key = os.getenv("OPENAI_API_KEY")
+print(api_key)
