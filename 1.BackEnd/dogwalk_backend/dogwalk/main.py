d9265dd9731eac368cc417a0810b16ddac6a9392
DESKTOP-4099OJD\USER <gywn1150@gmail.com>
2025-11-12 09:02:53 +0900

    한효주 - Main.py .env 수정

--- 1.BackEnd/dogwalk_backend/dogwalk/main.py
+++ 1.BackEnd/dogwalk_backend/dogwalk/main.py
@@ -1,10 +1,30 @@
+import os
+import json
+import redis
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-import os
 
-load_dotenv()  # 프로젝트 루트의 .env 파일 읽어서 환경변수 등록
+# Open API Key 불러오기
+os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("OPENAI_API_KEY")
-print(api_key)
+# 클래스 (pydantic 사용)
+
+
+# Fast API 메서드
+## LangChain 파이프라인 엔드포인트
+## POI 요청 엔드포인트
+## 챗봇 엔드포인트
+
+# LangChain 파이프라인
+
+
+# POI 요청 메서드
+
+
+# 챗봇 메서드
+# 입력 : 사용자 인풋 (str)
+# llm 챗봇 기능 : "role" : "프롬프트..."
